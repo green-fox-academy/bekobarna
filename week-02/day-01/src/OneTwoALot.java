deaef50ee2422e0dfcea3f6e07d56965335d9ecf
bekobarna <bocce15@gmail.com>
2017-09-11 16:43:02 +0200

    week02 day01 exercise 19 complete

--- week-02/day-01/src/OneTwoALot.java
+++ week-02/day-01/src/OneTwoALot.java
@@ -3,3 +3,24 @@
 // If the number is one it should print: One
 // If the number is two it should print: Two
 // If the number is more than two it should print: A lot
+
+import java.util.Scanner;
+
+public class OneTwoALot {
+    public static void main(String[] args) {
+        System.out.println("Please write a number here: ");
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+        if (number <= 0) {
+            System.out.println("zero");
+        } else if (number == 1) {
+            System.out.println("one");
+        }  else if (number == 2) {
+        System.out.println("two");
+        }  else if (number == 2) {
+            System.out.println("two");
+        } else {
+            System.out.println("A lot");
+        }
+    }
+}
