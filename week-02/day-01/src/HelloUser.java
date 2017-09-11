1403c201c71caf39ba28edeeb2a0b1e657082d4a
bekobarna <bocce15@gmail.com>
2017-09-11 16:15:26 +0200

    week02 day01 exercise14 complete

--- week-02/day-01/src/HelloUser.java
+++ week-02/day-01/src/HelloUser.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 public class HelloUser {
   public static void main(String[] args) {
     // Modify this program to greet user instead of the World!
     // The program should ask for the name of the user
-    System.out.println("Hello, World!");
+      System.out.println("Please write your name here: ");
+      Scanner scan = new Scanner (System.in);
+      String name = scan.nextLine();
+      System.out.println("Hello " + name);
+
   }
 }
