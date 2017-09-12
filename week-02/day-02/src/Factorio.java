41a85a2c376d6a69f35cb81fa830eacec619bfbc
bekobarna <bocce15@gmail.com>
2017-09-12 16:12:11 +0200

    factorio done, but needs polish

---
+++ week-02/day-02/src/Factorio.java
@@ -0,0 +1,23 @@
+//  Create the usual class wrapper
+//  and main method on your own.
+
+// - Create a function called `factorio`
+//   that returns it's input's factorial
+
+import java.util.Scanner;
+
+public class Factorio {
+    public static void main(String[] args) {
+        System.out.println("Please write a number here: ");
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+        System.out.println("The factorial of your number is: " + factorio(number));
+    }
+
+    public static int factorio (int number) {
+        for (int i = 0; i < number; i++) {
+            number = i * number;
+        }
+        return number;
+    }
+}
