33798f86e5e500d04b7c424f7216ec0e109aa412
bekobarna <bocce15@gmail.com>
2017-09-12 17:17:56 +0200

    PrintAll modified, Matrix started

---
+++ week-02/day-02/src/DiagonalMatrix.java
@@ -0,0 +1,19 @@
+// - Create (dynamically) a two dimensional array
+//   with the following matrix. Use a loop!
+//
+//   1 0 0 0
+//   0 1 0 0
+//   0 0 1 0
+//   0 0 0 1
+//
+// - Print this two dimensional array to the output
+
+public class DiagonalMatrix {
+    public static void main(String[] args) {
+        int[] af = {4, 5, 6, 7};
+        for (int i = 0; i < af.length; i++) {
+            System.out.println(af[i] + " ");
+
+    }
+
+}
--- week-02/day-02/src/PrintAll.java
+++ week-02/day-02/src/PrintAll.java
@@ -2,11 +2,21 @@
 //   with the following content: `[4, 5, 6, 7]`
 // - Print all the elements of `af`
 
-import java.util.Arrays;
-
 public class PrintAll {
+    public static void main(String[] args) {
+        int[] af = {4, 5, 6, 7};
+        for (int i = 0; i < af.length; i++) {
+        System.out.println(af[i] + " ");
+        }
+    }
+}
+
+/*
+ import java.util.Arrays;
+
+ public class PrintAll {
     public static void main(String[] args) {
     int[] af = {4, 5, 6, 7};
         System.out.println(Arrays.toString(af));
     }
-}
+} */
