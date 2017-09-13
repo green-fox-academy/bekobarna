91fd04fa158269b725a4e4c6b341be8b3c3e5242
bekobarna <bocce15@gmail.com>
2017-09-13 17:22:21 +0200

    QuoteSwap done

--- week-02/day-03/src/QuoteSwap.java
+++ week-02/day-03/src/QuoteSwap.java
@@ -15,10 +15,19 @@ public class QuoteSwap{
         list.remove(5);
         list.add(5, "do");
 
+        String one = list.get(0);
+        String two = list.get(1);
+        String three = list.get(2);
+        String four = list.get(3);
+        String five = list.get(4);
+        String six = list.get(5);
+        String seven = list.get(6);
+        String eight = list.get(7);
 
-        
-        for (int i = 0; i < list.size(); i++) {
-            System.out.println(list.get(i));
-        }
+        System.out.println(one + " " + two  + " " + three + " " + four + " " + five + " " + six + " " + seven + " " + eight);
     }
 }
+
+/*for (int i = 0; i < list.size(); i++) {
+          System.out.print(list.get(i));
+        }*/
