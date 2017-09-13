520122f9ae6e9e2ac862253944944d411c5be171
bekobarna <bocce15@gmail.com>
2017-09-13 13:53:45 +0200

    Solarsystem done

---
+++ week-02/day-03/src/SolarSystem.java
@@ -0,0 +1,12 @@
+// Saturn is missing from the planetList
+// Insert it into the correct position
+
+import java.util.*;
+
+public class SolarSystem{
+    public static void main(String... args){
+        ArrayList<String> planetList = new ArrayList<String>(Arrays.asList("Mercury","Venus","Earth","Mars","Jupiter","Uranus","Neptune"));
+        planetList.add(5, "Saturn");
+        System.out.println(planetList);
+    }
+}
