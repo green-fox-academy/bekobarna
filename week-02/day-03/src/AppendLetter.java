d76e945a7fcbfbd0f1918a9b45e9d4a76f934e79
bekobarna <bocce15@gmail.com>
2017-09-13 15:41:40 +0200

    Appendletter done

---
+++ week-02/day-03/src/AppendLetter.java
@@ -0,0 +1,16 @@
+// Add "a" to every string in the far list.
+
+import java.util.*;
+
+public class AppendLetter{
+    public static void main(String... args){
+        ArrayList<String> far = new ArrayList<String>(Arrays.asList("kuty", "macsk", "kacs", "rók", "halacsk"));
+        String a = "a";
+        for (int i = 0; i < far.size(); i++) {
+           String name = far.get(i);
+           name = name + "a";
+           far.set(i, name);
+        }
+        System.out.println(far);
+    }
+}
