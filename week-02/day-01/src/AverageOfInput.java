89f2db3b986bf2cdaeb9c6c9c966f536c42550b1
bekobarna <bocce15@gmail.com>
2017-09-14 10:27:01 +0200

    Day-01 AverageOfInput done

--- week-02/day-01/src/AverageOfInput.java
+++ week-02/day-01/src/AverageOfInput.java
@@ -2,3 +2,26 @@
 // then it should print the sum and the average of these numbers like:
 //
 // Sum: 22, Average: 4.4
+
+import java.util.Scanner;
+
+public class AverageOfInput {
+    public static void main(String[] args) {
+        System.out.println("Please write 5 numbers here: ");
+        Scanner scan1 = new Scanner (System.in);
+        int num1 = scan1.nextInt();
+        Scanner scan2 = new Scanner (System.in);
+        int num2 = scan2.nextInt();
+        Scanner scan3 = new Scanner (System.in);
+        int num3 = scan3.nextInt();
+        Scanner scan4 = new Scanner (System.in);
+        int num4 = scan4.nextInt();
+        Scanner scan5 = new Scanner (System.in);
+        int num5 = scan5.nextInt();
+        int sum = (num1 + num2 +num3 + num4 + num5);
+        int average = ((num1 + num2 +num3 + num4 + num5) / 5);
+        System.out.println("Sum: " + sum + ", " + "Average: " + average);
+
+    }
+}
+
