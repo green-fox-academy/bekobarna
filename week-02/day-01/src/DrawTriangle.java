22111f011ca5d06f01eb59dc055c992368b0fe5c
bekobarna <bocce15@gmail.com>
2017-09-14 11:46:42 +0200

    GuessTheNumber done

--- week-02/day-01/src/DrawTriangle.java
+++ week-02/day-01/src/DrawTriangle.java
@@ -15,7 +15,7 @@ public class DrawTriangle {
         Scanner scan = new Scanner(System.in);
         int number = scan.nextInt();
         for (int i = 0; i < (number); i++) {
-            for (int j=0; j<=i; j++){
+            for (int j = 0; j <= i; j++){
                 System.out.print("*");
             }
             System.out.println("");
--- week-02/day-01/src/GuessTheNumber.java
+++ week-02/day-01/src/GuessTheNumber.java
@@ -5,3 +5,27 @@
 // The stored number is higher
 // The stried number is lower
 // You found the number: 8
+
+import java.util.Scanner;
+public class GuessTheNumber {
+    public static void main(String[] args) {
+        while(true) {
+            int numberStored = 99;
+            int numberOfTries = 0;
+            while (true) {
+                System.out.println("Please enter a number: ");
+                Scanner scan = new Scanner(System.in);
+                int guess = scan.nextInt();
+                numberOfTries++;
+
+                if (guess == numberStored) {
+                    System.out.println("You found the number: " + numberStored);
+                    // leave the first loop
+                    break;
+                }
+                else if (guess < numberStored) System.out.println("The stored number is lower");
+                else if (guess > numberStored) System.out.println("The stored number is higher");
+            }
+        }
+    }
+}
