eaab8cccf255c834294e384eb2751a10fd6b858b
bekobarna <bocce15@gmail.com>
2017-09-14 11:18:00 +0200

    DrawTriangle done

--- week-02/day-01/src/DrawTriangle.java
+++ week-02/day-01/src/DrawTriangle.java
@@ -7,3 +7,18 @@
 // ****
 //
 // The triangle should have as many lines as the number was
+
+import java.util.Scanner;
+public class DrawTriangle {
+    public static void main(String[] args) {
+        System.out.println("Please write a number here: ");
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+        for (int i = 0; i < (number); i++) {
+            for (int j=0; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
