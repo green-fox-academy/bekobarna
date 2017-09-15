cef75fd73ab65d6922fb607b95b882e9052a0da6
bekobarna <bocce15@gmail.com>
2017-09-15 09:51:58 +0200

    GuessMyNumber done

--- week-02/day-05/src/GuessMyNumber.java
+++ week-02/day-05/src/GuessMyNumber.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
 public class GuessMyNumber {
+  public static void main(String[] args) {
+    System.out.println("Welcome, we will play a little guessing game.");
+    System.out.println("Please tell me the starting number: ");
+    Scanner scan = new Scanner(System.in);
+    int numStart = scan.nextInt();
+    System.out.println("Please tell me the ending number");
+    Scanner scan2 = new Scanner(System.in);
+    int numEnd = scan2.nextInt();
+    System.out.println("Awesome, let`s start!");
 
+    int numberStored = getRandomNumber(numStart, numEnd);
+    while (true) {
+      System.out.println("Please enter a number between " + numStart + " and " + numEnd + ":");
+      Scanner scan3 = new Scanner(System.in);
+      int guess = scan3.nextInt();
+      if (guess == numberStored) {
+        System.out.println("Congratulations, you found the number: " + numberStored);
+        break;
+      }
+      else if (guess > numberStored) System.out.println("The stored number is lower");
+      else  System.out.println("The stored number is higher");
+    }
+  }
 
-  
+  static int getRandomNumber(int from, int to) {
+    return (int) (Math.random() * (to - from) + from);
+  }
 }
