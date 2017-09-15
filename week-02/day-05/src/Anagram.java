670bb4da8d7d2ddbf82e2a8de8d3144fc64543fc
bekobarna <bocce15@gmail.com>
2017-09-15 11:45:33 +0200

    Anagram done

--- week-02/day-05/src/Anagram.java
+++ week-02/day-05/src/Anagram.java
@@ -5,20 +5,22 @@ public class Anagram {
     System.out.println("Please tell me a word");
     Scanner scan = new Scanner(System.in);
     String firstWord = scan.nextLine();
-    ArrayList<String> firstList = new ArrayList<>();
-    firstList.add(firstWord);
+    char[] chars = firstWord.toCharArray();
+    Arrays.sort(chars);
+    String firstWordSorted = new String(chars);
 
     System.out.println("Please tell me another word");
     String secondWord = scan.nextLine();
-    ArrayList<String> secondList = new ArrayList<>();
-    secondList.add(secondWord);
+    char[] chars2 = secondWord.toCharArray();
+    Arrays.sort(chars2);
+    String secondWordSorted = new String(chars);
 
     System.out.println("");
-    System.out.println(isAnagram(firstList, secondList));
+    System.out.println(isAnagram(firstWordSorted, secondWordSorted));
   }
 
-  public static boolean isAnagram (String[] first, String[] second) {
-    if (first.containsAll(second))
+  public static boolean isAnagram (String first, String second) {
+    if ((first.length() == second.length()) && (first.equalsIgnoreCase(second)))
       return true;
     else {
       return false;
