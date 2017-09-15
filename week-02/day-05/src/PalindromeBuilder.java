ab679bef34b4cd01b02de4b41dd73a5d70baba4d
bekobarna <bocce15@gmail.com>
2017-09-15 13:09:15 +0200

    PalindromeBuilder done

--- week-02/day-05/src/PalindromeBuilder.java
+++ week-02/day-05/src/PalindromeBuilder.java
@@ -2,9 +2,17 @@ import java.util.*;
 
 public class PalindromeBuilder {
   public static void main(String[] args) {
-
-
-
-
+    System.out.println("Please write a word here:");
+    Scanner scan = new Scanner(System.in);
+    String wordInput = scan.nextLine();
+    System.out.println("");
+    System.out.println(createPalindrome(wordInput));
   }
+
+  public static String createPalindrome (String word) {
+      StringBuilder word2 = new StringBuilder(word);
+      word2.reverse();
+      word = word + word2;
+      return word;
+    }
 }
