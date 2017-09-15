b134f7eec42fdb53f9187ed9f102af569c03e743
bekobarna <bocce15@gmail.com>
2017-09-15 10:04:21 +0200

    Anagram started

---
+++ week-02/day-05/src/Anagram.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Anagram {
+  public static void main(String[] args) {
+    System.out.println("Please tell me a word");
+    Scanner scan = new Scanner(System.in);
+    String firstWord = scan.nextLine();
+    System.out.println("Please tell me another word");
+    Scanner scan2 = new Scanner(System.in);
+    String secondWord = scan2.nextLine();
+
+
+
+  }
+
+  static boolean isAnagram (String word1, String word2);
+
+
+
+}
--- week-02/day-05/src/GuessMyNumber.java
+++ week-02/day-05/src/GuessMyNumber.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
 public class GuessMyNumber {
   public static void main(String[] args) {
-    System.out.println("Welcome, we will play a little guessing game.");
+    System.out.println("Welcome, we will play a little guessing game");
     System.out.println("Please tell me the starting number: ");
     Scanner scan = new Scanner(System.in);
     int numStart = scan.nextInt();
     System.out.println("Please tell me the ending number");
     Scanner scan2 = new Scanner(System.in);
     int numEnd = scan2.nextInt();
+    System.out.println("");
     System.out.println("Awesome, let`s start!");
 
     int numberStored = getRandomNumber(numStart, numEnd);
