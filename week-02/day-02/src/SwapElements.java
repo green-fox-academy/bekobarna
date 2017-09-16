c36cab33c049efa8b1e3f9458d7878dd006690b8
bekobarna <bocce15@gmail.com>
2017-09-16 13:03:38 +0200

    SwapElements done

---
+++ week-02/day-02/src/SwapElements.java
@@ -0,0 +1,17 @@
+// - Create an array variable named `abc`
+//   with the following content: `["first", "second", "third"]`
+// - Swap the first and the third element of `abc`
+
+public class SwapElements {
+  public static void main(String[] args) {
+    String[] abc = {"first", "second", "third"};
+    String temp1 = abc[0];
+    String temp2 = abc[2];
+    abc[0] = temp2;
+    abc[2] = temp1;
+
+    for (int i = 0; i < abc.length; i++) {
+      System.out.println(abc[i]);
+    }
+  }
+}
