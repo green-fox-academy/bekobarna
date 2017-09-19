0d39a0b803497d31e750bd3a06a75a505a680b59
bekobarna <bocce15@gmail.com>
2017-09-19 11:32:35 +0200

    CountLines started

--- week-03/day-02/src/CountLines.java
+++ week-03/day-02/src/CountLines.java
@@ -2,3 +2,30 @@
 // then returns the number of lines the file contains.
 // It should return zero if it can't open the file, and
 // should not raise any error.
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.util.List;
+
+public class CountLines {
+  public static void main(String[] args) {
+
+    }
+  }
+
+  public static String numberOfLines (String fileName){
+    Path filePath = Paths.get("my-file.txt");
+    try {
+      List<String> myList = Files.readAllLines(filePath);
+    } catch (IOException e) {
+      System.out.println(0);
+    }
+    return ;
+
+  }
+
+
+}
