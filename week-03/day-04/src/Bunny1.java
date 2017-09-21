50f51277c0d0bea72e53f3c6e8b001249029d816
bekobarna <bocce15@gmail.com>
2017-09-21 12:18:31 +0200

    Bunny1 done

--- week-03/day-04/src/Bunny1.java
+++ week-03/day-04/src/Bunny1.java
@@ -1,3 +1,22 @@
 // We have a number of bunnies and each bunny has two big floppy ears.
 // We want to compute the total number of ears across all the bunnies recursively (without loops or multiplication).
 
+import java.util.Scanner;
+
+public class Bunny1 {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("How many bunnies do you want:");
+    int number = scan.nextInt();
+
+    System.out.println(bunnies(number));
+  }
+
+  public static int bunnies(int n) {
+    if (n == 0) {
+      return 0;
+    } else {
+      return  2 + bunnies(n - 1);
+    }
+  }
+}
