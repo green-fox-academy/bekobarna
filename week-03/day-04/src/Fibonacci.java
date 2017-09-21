e6f2afeaa53ee5f189ca8fab44166388b4bde8c3
bekobarna <bocce15@gmail.com>
2017-09-21 15:20:23 +0200

    Fibonacci done

--- week-03/day-04/src/Fibonacci.java
+++ week-03/day-04/src/Fibonacci.java
@@ -4,3 +4,24 @@
 // previous two values, so the whole sequence is: 0, 1, 1, 2, 3, 5, 8, 13, 21
 // and so on. Define a recursive fibonacci(n) method that returns the nth
 // fibonacci number, with n=0 representing the start of the sequence.
+
+import java.util.Scanner;
+
+public class Fibonacci {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Please tell me a number:");
+    int number = scan.nextInt();
+    System.out.println(fibo(number));
+  }
+
+  static int fibo(int n) {
+    if (n == 0) {
+      return 0;
+    } else if (n == 1) {
+      return 1;
+    } else {
+      return (fibo(n - 1) + fibo( n - 2));
+    }
+  }
+}
--- week-03/day-04/src/String3.java
+++ week-03/day-04/src/String3.java
@@ -1,2 +1,22 @@
 // Given a string, compute recursively a new string where all the
 // adjacent chars are now separated by a "*".
+
+import java.util.Scanner;
+
+public class String3 {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Write me a word with x`s:");
+    String inputString = scan.nextLine();
+
+    System.out.println(string(inputString));
+  }
+
+  public static String string(String word) {
+    if (word.contains("x") == false) {
+      return word;
+    }  else {
+      return string(word.replace("x", ""));
+    }
+  }
+}
