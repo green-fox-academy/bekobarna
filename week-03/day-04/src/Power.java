115f333b350e5a4586d5d0d3d564555afca417dd
bekobarna <bocce15@gmail.com>
2017-09-21 12:11:05 +0200

    Power done

--- week-03/day-04/src/Power.java
+++ week-03/day-04/src/Power.java
@@ -1,2 +1,22 @@
 // Given base and n that are both 1 or more, compute recursively (no loops)
 // the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
+
+import java.util.Scanner;
+
+public class Power {
+  public static void main(String[] args) {
+      Scanner scan = new Scanner(System.in);
+      System.out.println("Please tell me two number:");
+      int number1 = scan.nextInt();
+      int number2 = scan.nextInt();
+      System.out.println(powerN(number1, number2));
+  }
+
+  public static int powerN(int n, int base) {
+      if ((n == 0) || (base == 0)) {
+        return 1;
+      } else {
+        return  n * powerN(n, base - 1);
+      }
+  }
+}
--- week-03/day-04/src/SumDigit.java
+++ week-03/day-04/src/SumDigit.java
@@ -1,3 +1,24 @@
 // Given a non-negative int n, return the sum of its digits recursively (no loops). 
 // Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while 
 // divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+
+import java.util.Scanner;
+
+public class SumDigit {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Please tell me a number:");
+    int number = scan.nextInt();
+    System.out.println(sum(number));
+  }
+
+  public static int sum(int n) {
+    if ((n % 10) == n) {
+      return n;
+    } else {
+      int rightMost = n % 10;
+      return  rightMost + sum(n / 10);
+    }
+  }
+
+}
