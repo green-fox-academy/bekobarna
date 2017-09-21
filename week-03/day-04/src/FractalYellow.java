d3870597a54fb7450d5ac75b3a303ab53cb2449a
bekobarna <bocce15@gmail.com>
2017-09-21 21:49:52 +0200

    FractalYellow polished a little, basics done

--- week-03/day-04/src/FractalYellow.java
+++ week-03/day-04/src/FractalYellow.java
@@ -10,45 +10,33 @@ public class FractalYellow {
     graphics.fillRect(0, 0, 1000, 1000);
     graphics.setColor(Color.BLACK);
 
-    int x = 10;
-    int y = 10;
-    int size = HEIGHT;
 
-    for (int i = 0; i < 20; i++) {
+    int size = 450;
+    int n = 3;                  //not sure about the number
+    int x = (WIDTH - size) / 2;
+    int y = (HEIGHT - size) / 2;
+
+    squareDraw(graphics, x, y, size, n);
+
+
+    /*for (int i = 0; i < 20; i++) {
         squareDraw(graphics, 10 * i, 10 * i, size);
         size = size / 2;
         x += 10;
-        y += 10;
+        y += 10;*/
 
-      }
-
-   /* if (n < 100) {
-      squareDraw(graphics, x , y, size, n);
-    } else {
-
-    }*/
-
-  }
+    }
 
 
-  static void squareDraw(Graphics graphics, int x, int y, int size){
-    size = WIDTH / 3;
+  static void squareDraw(Graphics graphics, int x, int y, int size, int n){
 
-    graphics.drawLine(0, HEIGHT / 3, HEIGHT, HEIGHT / 3 );
-    graphics.drawLine(0, (2 * HEIGHT / 3), HEIGHT, (2 * HEIGHT / 3));
-    graphics.drawLine(WIDTH / 3, 0, HEIGHT / 3, HEIGHT );
-    graphics.drawLine(2 * WIDTH/3, 0, 2 * WIDTH / 3, HEIGHT);
+    graphics.drawRect(x , y , size, size);
+    graphics.drawRect(x + size / 3, y, size / 3, size / 3);
+    graphics.drawRect(x, y + size / 3 , size / 3, size / 3);
+    graphics.drawRect(x + size / 3, y + size * 2 / 3, size / 3, size / 3);
+    graphics.drawRect(x + size * 2 / 3, y +  size / 3, size / 3, size /3);
 
-    /*graphics.drawRect(x , y , size, size);
-    graphics.drawRect(x + size, y + size, size, size);
-    graphics.drawRect(x + 2*size, y + 2*size , size, size);
-    graphics.drawRect(x + 2*size, y, size, size);
-    graphics.drawRect(x + size, y, size, size);
-    graphics.drawRect(x + 2*size , y + 2*size , size, size);
-    graphics.drawRect(x , y + size, size, size);
-    graphics.drawRect(x , y + 2*size, size, size);
-    graphics.drawRect(x + 2*size, y + size, size, size);
-    graphics.drawRect(x +size , y + 2*size, size, size);*/
+
   }
 
 
