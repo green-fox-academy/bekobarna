04da1d852414f044f4c355abbb79cfa9e9e05df9
bekobarna <bocce15@gmail.com>
2017-09-21 12:53:44 +0200

    String1 kinda done

--- week-03/day-04/src/String1.java
+++ week-03/day-04/src/String1.java
@@ -1,2 +1,22 @@
 // Given a string, compute recursively (no loops) a new string where all the
 // lowercase 'x' chars have been changed to 'y' chars.
+
+import java.util.Scanner;
+
+public class String1 {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Write me a string:");
+    String inputString = scan.nextLine();
+    System.out.println(string(inputString));
+
+  }
+
+  public static String string(String word) {
+    if (word.contains("x") == false) {
+      return word;
+    }  else {
+      return string(word.replace("x", "y"));
+    }
+  }
+}
