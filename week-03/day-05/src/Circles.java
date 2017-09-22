3a0e8b0c521ceb51eecd330ae94d768c11cf7f64
bekobarna <bocce15@gmail.com>
2017-09-22 15:29:41 +0200

    Circles fixed

--- week-03/day-05/src/Circles.java
+++ week-03/day-05/src/Circles.java
@@ -5,44 +5,40 @@ import static javax.swing.JFrame.EXIT_ON_CLOSE;
 public class Circles {
 
   public static void mainDraw(Graphics graphics) {
-    graphics.setColor(new Color(255, 255, 255));
-    graphics.fillRect(0, 0, 1500, 800);
-
-   // int sizeRandom = (int) (Math.random( ) * 1000);        //random size
-    int size = 800;
-    int n = 3;
-    int x = (WIDTH - size) / 3;
-    int y = (HEIGHT - size) / 3;
-
-    drawCircle(graphics, x, y, size, n);
+      graphics.setColor(new Color(255, 255, 255));
+      graphics.fillRect(0, 0, 1500, 800);
+     /* int sizeRandom = (int) (Math.random( ) * 1000);                 //random size
+      int size = sizeRandom;*/
+      int size = 900;                                                    // fix size
+      int n = 2;                                                     // how deep do you want to go?
+      int x = (WIDTH - size) / 2;
+      int y = (HEIGHT - size) / 2;
+      drawCircle(graphics, x, y, size, n);
   }
 
-
   static void drawCircle(Graphics graphics, int x, int y, int size, int n){
-    int red = (int) (Math.random( ) * 256);
-    int green = (int) (Math.random() * 256);
-    int blue = (int) (Math.random() * 256);
+      int red = (int) (Math.random( ) * 256);                 // random colors
+      int green = (int) (Math.random() * 256);                // random colors
+      int blue = (int) (Math.random() * 256);                 // random colors
 
-    if (n < 1){
-      return;
-    }
+      if (n < 1){
+        return;
+      }
+        graphics.setColor(new Color(red, green, blue));
+        graphics.drawOval(x , y , size, size);
 
-    graphics.setColor(new Color(red, green, blue));
-    graphics.drawOval(x , y , size, size);
+        graphics.setColor(new Color(red, green, blue));
+        graphics.drawOval(x + size / 4, y, size / 2, size / 2);
 
-    graphics.setColor(new Color(red, green, blue));
-    graphics.drawOval(x + size / 4, y, size / 2, size / 2);
+        graphics.setColor(new Color(red, green, blue));
+        graphics.drawOval((int) (x + size / 31.5), (int) (y + size / 2.71), size / 2, size / 2);
 
-    graphics.setColor(new Color(red, green, blue));
-    graphics.drawOval((int) (x + size / 31.5), (int) (y + size / 2.71), size / 2, size / 2);
+        graphics.setColor(new Color(red, green, blue));
+        graphics.drawOval((int) (x + size / 2.138), (int) (y + size / 2.69), size / 2, size / 2);
 
-    graphics.setColor(new Color(red, green, blue));
-    graphics.drawOval((int) (x + size / 2.138), (int) (y + size / 2.69), size / 2, size / 2);
-
-
-    drawCircle(graphics, x + size / 4, y, size / 2, n - 1);
-    drawCircle(graphics, (int) (x + (size / 30.4)), (int) (y + size / 2.70), size / 2, n - 1);
-    drawCircle(graphics, (int) (x + (size / 2.14)), (int) (y + size / 2.70), size / 2, n - 1);
+        drawCircle(graphics, x + size / 4, y, size / 2, n - 1);
+        drawCircle(graphics, (int) (x + (size / 30.4)), (int) (y + size / 2.70), size / 2, n - 1);
+        drawCircle(graphics, (int) (x + (size / 2.14)), (int) (y + size / 2.70), size / 2, n - 1);
   }
 
 
