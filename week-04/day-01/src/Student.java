e14d0862e5e398d3782545293095130cfaf7f781
bekobarna <bocce15@gmail.com>
2017-09-25 20:11:39 +0200

    Student and Teacher done

---
+++ week-04/day-01/src/Student.java
@@ -0,0 +1,13 @@
+public class Student {
+    public void learn() {
+    }
+
+    public void question (Teacher teacher) {
+        teacher.answer();
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ week-04/day-01/src/Teacher.java
@@ -0,0 +1,13 @@
+public class Teacher {
+    public void answer() {
+    }
+
+    public void teach (Student student) {
+        student.learn();
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
