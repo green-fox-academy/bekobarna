724dc8e0bf689ee930500b8c1d246ee8da7b18df
bekobarna <bocce15@gmail.com>
2017-09-26 14:02:57 +0200

    First exercise / Mentor done

---
+++ week-04/day-02/src/Mentor.java
@@ -0,0 +1,26 @@
+public class Mentor extends Person {
+    String level;
+
+    public Mentor () {
+        name = "Jane Doe";
+        age = 30;
+        gender = "female";
+        level = "intermediate";
+    }
+
+    public Mentor(String name, int age, String gender, String level) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.level = level;
+    }
+
+    public void getGoal() {
+        System.out.println( "Educate brilliant junior software developers.");
+    }
+
+    public void introduce () {
+        System.out.println("Hi, I'm " + name + ", a " + age + " year old " + gender + " " + level + " mentor");
+    }
+
+}
