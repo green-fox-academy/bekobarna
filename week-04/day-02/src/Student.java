10ddd69facb594de8401a7722258140de0be494c
bekobarna <bocce15@gmail.com>
2017-09-26 13:56:29 +0200

    First exercise / Student done

--- week-04/day-02/src/Student.java
+++ week-04/day-02/src/Student.java
@@ -2,13 +2,32 @@ public class Student extends Person {
     String previousOrganization;
     int skippedDays;
 
+    public Student (String name, int age, String gender, String previousOrganization) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.previousOrganization = previousOrganization;
+    }
+
+    public Student() {
+        name = "Jane doe";
+        age = 30;
+        gender = "female";
+        previousOrganization = "The School of Life";
+        skippedDays = 0;
+    }
+
+
     public void getGoal() {
         System.out.println("Be a junior software developer.");
     }
 
     public void introduce () {
-        System.out.println("Hi, I'm " + name + ", a " + age + " year old " + gender + ".");
+        System.out.println("Hi, I'm " + name + ", a " + age + " year old " + gender + " from " + previousOrganization + " who skipped " + skippedDays + " days from the course already.");
+    }
 
+    public void skipDays (int numberOfDays) {
+        skippedDays += numberOfDays;
     }
 
 
