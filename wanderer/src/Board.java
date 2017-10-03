d16c7fc02c68662f5b19c687fe7370b79a1e2f86
bekobarna <bocce15@gmail.com>
2017-10-03 12:28:16 +0200

    Wanderer at different place

---
+++ wanderer/src/Board.java
@@ -0,0 +1,84 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class Board extends JComponent implements KeyListener {
+
+    int testBoxX;
+    int testBoxY;
+
+    public Board() {
+        testBoxX = 300;
+        testBoxY = 300;
+
+        // set the size of your draw board
+        setPreferredSize(new Dimension(720, 720));
+        setVisible(true);
+    }
+
+    @Override
+    public void paint(Graphics graphics) {
+        super.paint(graphics);
+
+        testBoxX = 0;
+        testBoxY = 0;
+        PositionedImage tile;
+
+        for (int y = 0; y < 10; y++) {
+            for (int x = 0; x < 10; x++) {
+                tile = new PositionedImage("assets/floor.png", testBoxX, testBoxY);
+                tile.draw(graphics);
+                testBoxY += 70;
+            }
+            tile = new PositionedImage("assets/floor.png", testBoxX, 0);
+            tile.draw(graphics);
+           /* testBoxX += 70;*/
+        }
+
+        // here you have a 720x720 canvas
+        // you can create and draw an image using the class below e.g.
+        PositionedImage hero = new PositionedImage("assets/hero-down.png", 0, 0);
+        hero.draw(graphics);
+    }
+
+    public static void main(String[] args) {
+        // Here is how you set up a new window and adding our board to it
+        JFrame frame = new JFrame("RPG Game");
+        Board board = new Board();
+        frame.add(board);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.pack();
+        // Here is how you can add a key event listener
+        // The board object will be notified when hitting any key
+        // with the system calling one of the below 3 methods
+        frame.addKeyListener(board);
+        // Notice (at the top) that we can only do this
+        // because this Board class (the type of the board object) is also a KeyListener
+    }
+
+    // To be a KeyListener the class needs to have these 3 methods in it
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    // But actually we can use just this one for our goals here
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // When the up or down keys hit, we change the position of our box
+        if (e.getKeyCode() == KeyEvent.VK_UP) {
+            testBoxY -= 100;
+        } else if(e.getKeyCode() == KeyEvent.VK_DOWN) {
+            testBoxY += 100;
+        }
+        // and redraw to have a new picture with the new coordinates
+        repaint();
+    }
+}
