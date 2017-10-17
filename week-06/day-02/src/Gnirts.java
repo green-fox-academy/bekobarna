5d662ffbc58951788244d99e90014f146b6561eb
bekobarna <bocce15@gmail.com>
2017-10-17 12:57:34 +0200

    Gnirts done

---
+++ week-06/day-02/src/Gnirts.java
@@ -0,0 +1,28 @@
+public class Gnirts implements CharSequence {
+    String word;
+
+    public Gnirts(String input) {
+        this.word = input;
+    }
+
+    @Override
+    public int length() {
+        return word.length();
+    }
+
+    @Override
+    public char charAt(int index) {
+        return this.word.charAt(word.length() - 1 - index);
+    }
+
+    @Override
+    public CharSequence subSequence(int start, int end) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Gnirts g = new Gnirts("example");
+        System.out.println(g.charAt(1));
+        // should print out: l
+    }
+}
---
+++ week-06/day-02/src/Shifter.java
@@ -0,0 +1,18 @@
+public class Shifter implements CharSequence {
+    
+
+    @Override
+    public int length() {
+        return 0;
+    }
+
+    @Override
+    public char charAt(int index) {
+        return 0;
+    }
+
+    @Override
+    public CharSequence subSequence(int start, int end) {
+        return null;
+    }
+}
