fdeadaf737bf77822394cb0833a980072e3f69b6
bekobarna <bocce15@gmail.com>
2017-10-19 17:08:17 +0200

    SortTheArray done

--- week-06/day-04/src/SortTheArray.java
+++ week-06/day-04/src/SortTheArray.java
@@ -1,22 +1,46 @@
+import java.util.Arrays;
+
 public class SortTheArray {
 
         public static <T extends Number> void printArray(T[] inputArray) {
-            // Display array elements
-            for (T element : inputArray) {
-                System.out.printf("%s ", element);
+            try {
+                Arrays.sort(inputArray);
+            } catch (Exception e){
+                System.out.println("oops, something went wrong");
+            } finally {
+                Arrays.sort(inputArray);
+                for (T element : inputArray) {
+                    System.out.printf("%s ", element);
+                }
+                System.out.println();
             }
-            System.out.println();
         }
 
 
     public static void main(String[] args) {
-        Integer[] intArray = { 1, 2, 3, 4, 5 };
-        Double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
-        Character[] charArray = { 'H', 'E', 'L', 'L', 'O' };
+        Integer[] intArray = {5, 3, 1, 4, 2, 2 };
+        Double[] doubleArray = { 1.54, 2.55, 2.505, 4.56 };
+        Long[] longArray = { 999L, 2345L, 3L, 12897L, 151515L };
+        Float[] floatArray = { 5.4f, 2.9f, 2.8f, 0.1f, 5.0f };
+        Short[] shortArray = { 2, 8, 3, 0, 6};
+        Byte[] byteArray = {2, 7, 4, 10, 1};
 
+        System.out.println("Array integerArray contains:");
+        printArray(intArray);
 
+        System.out.println("\nArray doubleArray contains:");
+        printArray(doubleArray);
 
+        System.out.println("\nArray longArray contains:");
+        printArray(longArray);
 
+        System.out.println("\nArray floatArray contains:");
+        printArray(floatArray);
+
+        System.out.println("\nArray shortArray contains:");
+        printArray(shortArray);
+
+        System.out.println("\nArray byteArray contains:");
+        printArray(byteArray);
     }
-
 }
