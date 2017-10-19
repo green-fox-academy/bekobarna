b85947563e195de4779b0277a485191a0cbadf7f
bekobarna <bocce15@gmail.com>
2017-10-19 16:43:09 +0200

    StringSplitter done

--- week-06/day-04/src/SplitString.java
+++ week-06/day-04/src/SplitString.java
@@ -1,3 +1,22 @@
+import java.util.Arrays;
+
 public class SplitString {
 
+    public static void main(String[] args) {
+        int splitNumber = 4;
+        String inputString = "lovebarbara";
+
+        try {
+            System.out.println(Arrays.toString(stringSplitter(inputString, splitNumber)));
+        } catch(IndexOutOfBoundsException ex) {
+            System.out.println("Your number is bigger than the lenght of your word");
+        }
+    }
+
+    public static String[] stringSplitter (String input, int number) {
+        String[] array = new String[2];
+        array[0] = input.substring(0, number);
+        array[1] = input.substring(number, input.length());
+        return array;
+    }
 }
