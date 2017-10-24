01af1fe39658b60acaec9986d6e510d8320babfc
bekobarna <bocce15@gmail.com>
2017-10-24 13:55:50 +0200

    Hello user REST app done

--- week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRestControllers.java
+++ week-07/day-01/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRestControllers.java
@@ -1,14 +1,17 @@
 package com.greenfoxacademy.springstart.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloRestControllers {
 
+    @ResponseBody
     @RequestMapping(value = "/greeting")
-    public Greetings greeting () {
-        Greetings greetings = new Greetings(22, "Hello, you!");
+    public Greetings greeting (@RequestParam(value = "name") String name) {
+        Greetings greetings = new Greetings(1, name);
         return greetings;
     }
 }
