30eb00a5e999d45149611dc73a20ba3e5df20a13
bekobarna <bocce15@gmail.com>
2017-11-01 11:15:14 +0100

    ToDo with MySQL, delete and update todo in progress

--- week-08/day-02/todomysql/src/main/java/com/greenfox/todo/controller/ToDoController.java
+++ week-08/day-02/todomysql/src/main/java/com/greenfox/todo/controller/ToDoController.java
@@ -2,10 +2,13 @@ package com.greenfox.todo.controller;
 
 import com.greenfox.todo.model.ToDo;
 import com.greenfox.todo.repository.ToDoRepository;
+import com.sun.org.apache.xpath.internal.operations.Mod;
+import javax.jws.WebParam.Mode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -43,5 +46,26 @@ public class ToDoController {
     toDoRepository.save(new ToDo(title));
     return "redirect:/todo/";
     }
+
+    @RequestMapping(value = "/{id}/delete")
+    public String delete(Model model, @PathVariable Integer id) {
+        toDoRepository.delete(id);
+        model.addAttribute("inputid", id);
+        return "redirect:/todo/";
+    }
+
+    @RequestMapping(value = "/{id}/edit", method = RequestMethod.GET)
+    public String getEdit(Model model, @PathVariable Integer id) {
+        model.addAttribute("editid", toDoRepository.findOne(id));
+        return "edit";
+    }
+
+    @RequestMapping(value = "/{id}/edit", method = RequestMethod.POST)
+    public String edit(Model model, @PathVariable Integer id) {
+
+        model.addAttribute("save", toDoRepository);
+        return "redirect:/todo/";
+    }
+
 
 }
